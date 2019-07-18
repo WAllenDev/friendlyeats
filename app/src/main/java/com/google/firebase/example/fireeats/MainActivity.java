e79b745a4aa9713e1b3dc60f77e39101acff7712
Will Allen <wallenprog@gmail.com>
2019-07-18 10:07:13 -0700

    "first steps of tutorial" 	modified:   app/build.gradle 	modified:   app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java 	modified:   app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.java 	modified:   build.gradle

--- app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
+++ app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
@@ -36,7 +36,10 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.example.fireeats.adapter.RestaurantAdapter;
+import com.google.firebase.example.fireeats.model.Restaurant;
+import com.google.firebase.example.fireeats.util.RestaurantUtil;
 import com.google.firebase.example.fireeats.viewmodel.MainActivityViewModel;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -100,7 +103,10 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void initFirestore() {
-        // TODO(developer): Implement
+        mFirestore = FirebaseFirestore.getInstance();
+
+        // Get the 50 highest rated restaurants
+        mQuery = mFirestore.collection("restaurants").orderBy("avgRating", Query.Direction.DESCENDING).limit(LIMIT);
     }
 
     private void initRecyclerView() {
@@ -162,7 +168,16 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void onAddItemsClicked() {
-        // TODO(developer): Add random restaurants
+        // Get a reerence to the restaurants collection
+        CollectionReference restaurants = mFirestore.collection("restaurant");
+
+        for (int i = 0; i < 10; i++) {
+            // Get a random Restaurant POJO
+            Restaurant restaurant = RestaurantUtil.getRandom(this);
+
+            // Add a new document to the restaurant collection
+            restaurants.add(restaurant);
+        }
         showTodoToast();
     }
 
